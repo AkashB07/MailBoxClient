e40d84ccf1fcdc026055b0efe58f014b9c3c3045
AkashB07 <akashbadhi7@gmail.com>
2023-05-10 17:00:16 +0530

    Read Messages properly

--- react-mail-box-client/src/component/Inbox/Inbox.js
+++ react-mail-box-client/src/component/Inbox/Inbox.js
@@ -1,32 +1,38 @@
-import { useEffect, useCallback } from "react";
-import { Table} from "react-bootstrap";
+import { useEffect, useCallback, useState } from "react";
+import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { Button } from "react-bootstrap";
 
 import { mailActions } from "../../store/mail-slice";
+import ViewMail from "../ViewMail/ViewMail";
 
 const url = 'http://localhost';
 
 const Inbox = () => {
     // const token = useSelector(state => state.auth.token);
-    const token =localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
     const dispatch = useDispatch();
     const { receivedMail } = useSelector((state) => state.mail);
+    let [nonRead, setNonRead] = useState(0);
     //   const senderMail = useSelector((state) => state.auth.email);
     //   const email = senderMail.replace("@", "").replace(".", "");
 
 
-    const getMailsHandler = useCallback(async (page) => {
+    const getMailsHandler = useCallback(async () => {
         try {
             const transformData = (mail) => {
                 const newData = [];
                 for (let key of mail) {
                     newData.push(key);
+                    if (key.isRead === 'false') {
+                        setNonRead(++nonRead);
+                    }
                 }
                 dispatch(mailActions.updateReceivedMail({ mail: newData }));
             };
-            console.log(token);
+
             const respone = await axios.get(`${url}:4000/mail/inbox`, { headers: { "Authorization": token } });
             transformData(respone.data.mails);
         }
@@ -40,13 +46,26 @@ const Inbox = () => {
         getMailsHandler();
     }, [getMailsHandler]);
 
+    const viewMailHandler = async (mail) => {
+        try {
+            dispatch(mailActions.viewMailHandle({ id: mail._id }));
+            setNonRead(nonRead);
+            await axios.patch(`${url}:4000/mail/updatemail`, { id: mail._id }, { headers: { "Authorization": token } });
+            getMailsHandler()
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="row justify-content-center">
             <h1 className="text-center">Inbox</h1><br /><br /><br />
             <div className="col-md-11" >
-                <Table striped bordered hover variant="dark">
+                <Table striped >
                     <thead>
                         <tr>
+                            <th><h5 className="fw-bold">unread - {nonRead}</h5></th>
                             <th><h5 className="fw-bold">Sender</h5></th>
                             <th><h5 className="fw-bold">Subject</h5></th>
                             <th><h5 className="fw-bold">Date</h5></th>
@@ -56,9 +75,27 @@ const Inbox = () => {
                         {receivedMail.map((mail) => {
                             return (
                                 <tr key={mail._id}>
+                                    <td >
+                                        <div
+                                            style={{
+                                                backgroundColor:  mail.isRead==='false'? "blue": "white",            
+                                                height: "10px",
+                                                width: "10px",
+                                                marginTop: "0px",
+                                                marginRight: "0%",
+                                                border: "1px solid black",
+                                            }}
+                                        ></div>
+                                    </td>
                                     <td>{mail.from}</td>
                                     <td>{mail.subject}</td>
                                     <td>{new Date(mail.createdAt).toDateString()}</td>
+                                    <td>
+                                        <Button variant="success" onClick={() => viewMailHandler(mail)}>
+                                            View
+                                        </Button>
+                                    </td>
+                                    <ViewMail mail={mail} type={"recevied"} />
                                 </tr>
                             );
                         })}
--- react-mail-box-client/src/store/mail-slice.js
+++ react-mail-box-client/src/store/mail-slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const mailSlice = createSlice({
   name: "mail",
-  initialState: { receivedMail: [], sentMail: [], viewMail: false, changed: false },
+  initialState: { receivedMail: [], sentMail: [], viewMail: false },
   reducers: {
     updateReceivedMail(state, action) {
       state.receivedMail = action.payload.mail;
@@ -10,26 +10,16 @@ const mailSlice = createSlice({
     updateSentMail(state, action) {
       state.sentMail = action.payload.mail;
     },
-    deleteReceivedMail(state, action) {
-      const id = action.payload.id;
-      state.receivedMail = state.receivedMail.filter((mail) => mail.id !== id);
-      state.changed = ! state.changed;
-    },
-    deleteSentMail(state, action) {
-      const id = action.payload.id;
-      state.sentMail = state.sentMail.filter((mail) => mail.id !== id);
-      state.changed = ! state.changed;
-    },
     viewMailHandle(state, action) {
       const newid = action.payload.id;
-      const index = state.receivedMail.findIndex((mail) => mail.id === newid);
+      const index = state.receivedMail.findIndex((mail) => mail._id === newid);
       state.receivedMail[index].isRead = true;
       state.viewMail = !state.viewMail;
     },
     mailHandler(state) {
       state.viewMail = !state.viewMail;   
     }
-  },
+  }
 });
 
 export const mailActions = mailSlice.actions;
